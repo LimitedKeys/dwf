67d0d08f0851bcff34e4142a3637a446b4d92145
Ricky <richardhob@gmail.com>
2025-08-28 17:47:51 -0700

    Continue working through AnalogIn

--- src/Dwf/Dll/Access.hs
+++ src/Dwf/Dll/Access.hs
@@ -92,6 +92,15 @@ fToDoubleDouble f = alloca (\a -> alloca (\b -> do
     return $ check (errorCode, (cA, cB))
     ))
 
+fToDoubleDoubleDouble :: (Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> IO CInt) -> IO (DwfResult (Double, Double, Double))
+fToDoubleDoubleDouble f = alloca (\a -> alloca (\b -> alloca (\c -> do
+    errorCode <- fromIntegral <$> f a b c
+    cA <- _coerce <$> peek a
+    cB <- _coerce <$> peek b
+    cC <- _coerce <$> peek c
+    return $ check (errorCode, (cA, cB, cC))
+    )))
+
 fToDoubleDoubleInt :: (Ptr CDouble -> Ptr CDouble -> Ptr CInt -> IO CInt) -> IO (DwfResult (Double, Double, Int))
 fToDoubleDoubleInt f = alloca (\a -> alloca (\b -> alloca (\c -> do
     errorCode <- fromIntegral <$> f a b c
@@ -100,6 +109,46 @@ fToDoubleDoubleInt f = alloca (\a -> alloca (\b -> alloca (\c -> do
     cC <- fromIntegral <$> peek c
     return $ check (errorCode, (cA, cB, cC))
     )))
+
+fToDoubleArrayN :: Int -> (Ptr CDouble -> CInt -> IO CInt) -> IO (DwfResult [Double])
+fToDoubleArrayN n f = allocaArray n (\a -> do
+    let n' = (fromIntegral n) :: CInt 
+    errorCode <- fromIntegral <$> f a n' 
+    raw <- peekArray n a 
+    let samples = map _coerce raw
+    return $ check (errorCode, samples))
+
+fToDoubleArrayIN :: Int -> Int -> (Ptr CDouble -> CInt -> CInt -> IO CInt) -> IO (DwfResult [Double])
+fToDoubleArrayIN i n f = allocaArray n (\a -> do
+    let i' = (fromIntegral i) :: CInt 
+    let n' = (fromIntegral n) :: CInt 
+    errorCode <- fromIntegral <$> f a i' n' 
+    raw <- peekArray n a 
+    let samples = map _coerce raw
+    return $ check (errorCode, samples))
+
+fTo2DoubleArrayN :: Int -> (Ptr CDouble -> Ptr CDouble -> CInt -> IO CInt) -> IO (DwfResult [(Double, Double)])
+fTo2DoubleArrayN n f = allocaArray n (\a -> allocaArray n (\b -> do
+    let n' = fromIntegral n
+    errorCode <- fromIntegral <$> f a b n'
+    rawA <- peekArray n a
+    let samplesA = map _coerce rawA
+    rawB <- peekArray n a
+    let samplesB = map _coerce rawB
+    return $ check (errorCode, zip samplesA samplesB)
+    ))
+
+fTo2DoubleArrayIN :: Int -> Int -> (Ptr CDouble -> Ptr CDouble -> CInt -> CInt -> IO CInt) -> IO (DwfResult [(Double, Double)])
+fTo2DoubleArrayIN i n f = allocaArray n (\a -> allocaArray n (\b -> do
+    let n' = fromIntegral n
+    let i' = fromIntegral i
+    errorCode <- fromIntegral <$> f a b i' n'
+    rawA <- peekArray n a
+    let samplesA = map _coerce rawA
+    rawB <- peekArray n a
+    let samplesB = map _coerce rawB
+    return $ check (errorCode, zip samplesA samplesB)
+    ))
 
 fToStringN :: Int -> (CString -> IO CInt) -> IO (DwfResult String)
 fToStringN n f = allocaBytes n (\raw -> do
@@ -123,6 +172,32 @@ fToIntInt f = alloca (\a -> alloca (\b -> do
     cB <- fromIntegral <$> peek b
     return $ check (errorCode, (cA, cB))
     ))
+
+fToIntIntInt :: (Ptr CInt -> Ptr CInt -> Ptr CInt -> IO CInt) -> IO (DwfResult (Int, Int, Int))
+fToIntIntInt f = alloca (\a -> alloca (\b -> alloca (\c -> do
+    errorCode <- fromIntegral <$> f a b c
+    cA <- fromIntegral <$> peek a
+    cB <- fromIntegral <$> peek b
+    cC <- fromIntegral <$> peek c
+    return $ check (errorCode, (cA, cB, cC))
+    )))
+
+fToIntArrayIN :: Int -> Int -> (Ptr CShort -> CInt -> CInt -> IO CInt) -> IO (DwfResult [Int])
+fToIntArrayIN i n f = allocaArray n (\a -> do 
+    let i' = fromIntegral i
+    let n' = fromIntegral n
+    errorCode <- fromIntegral <$> f a i' n'
+    raw <- peekArray n a 
+    let samples = map fromIntegral raw
+    return $ check (errorCode, samples))
+
+fToIntDoubleArray32 :: (Ptr CDouble -> Ptr CInt -> IO CInt) -> IO (DwfResult [Double])
+fToIntDoubleArray32 f = alloca (\n -> allocaArray 32 (\a -> do
+    errorCode <- fromIntegral <$> f a n 
+    n' <- fromIntegral <$> peek n
+    raw <- peekArray n' a
+    let samples = map _coerce raw
+    return $ check (errorCode, samples)))
 
 fToBool :: (Ptr CInt -> IO CInt) -> IO (DwfResult Bool)
 fToBool f = alloca (\a -> do
--- src/Dwf/Dll/Api.hs
+++ src/Dwf/Dll/Api.hs
@@ -1,6 +1,9 @@
 
 module Dwf.Dll.Api where
 
+import Foreign.C.Types
+import Data.Coerce (coerce)
+
 import Dwf.Dll.Wrap
 import Dwf.Dll.Access
 
@@ -165,3 +168,194 @@ analogIOChannelNodeSetInfo p q r = fToDoubleDoubleInt (fdwf_analog_io_channel_no
     where p' = fromIntegral p
           q' = fromIntegral q
           r' = fromIntegral r
+
+-- Analog In 
+
+analogInReset :: Int -> IO (DwfResult ())
+analogInReset p = fCall (fdwf_analog_in_reset p')
+    where p' = fromIntegral p
+
+analogInConfigure :: Int -> Int -> Int -> IO (DwfResult ())
+analogInConfigure p q r = fCall (fdwf_analog_in_configure p' q' r')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+          r' = fromIntegral r
+
+analogInChannelCount :: Int -> IO (DwfResult Int)
+analogInChannelCount p = fToInt (fdwf_analog_in_channel_count p')
+    where p' = fromIntegral p
+
+analogInFrequencyInfo :: Int -> IO (DwfResult (Double, Double))
+analogInFrequencyInfo p = fToDoubleDouble (fdwf_analog_in_frequency_info p')
+    where p' = fromIntegral p
+
+analogInChannelFilterInfo :: Int -> IO (DwfResult Int)
+analogInChannelFilterInfo p = fToInt (fdwf_analog_in_channel_filter_info p')
+    where p' = fromIntegral p
+
+analogInChannelFilterSet :: Int -> Int -> Int -> IO (DwfResult ())
+analogInChannelFilterSet p q r = fCall (fdwf_analog_in_channel_filter_set p' q' r')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+          r' = fromIntegral r
+
+analogInChannelFilterGet :: Int -> Int -> IO (DwfResult Int)
+analogInChannelFilterGet p q = fToInt (fdwf_analog_in_channel_filter_get p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInChannelEnableSet :: Int -> Int -> Int -> IO (DwfResult ())
+analogInChannelEnableSet p q r = fCall (fdwf_analog_in_channel_enable_set p' q' r')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+          r' = fromIntegral r
+
+analogInChannelEnableGet :: Int -> Int -> IO (DwfResult Int)
+analogInChannelEnableGet p q = fToInt (fdwf_analog_in_channel_enable_get p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInAcquisitionModeSet :: Int -> Int -> IO (DwfResult ())
+analogInAcquisitionModeSet p q = fCall (fdwf_analog_in_acquisition_mode_set p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInFrequencySet :: Int -> Double -> IO (DwfResult ())
+analogInFrequencySet p q = fCall (fdwf_analog_in_frequency_set p' q')
+    where p' = fromIntegral p
+          q' = coerce q :: CDouble
+
+analogInFrequencyGet :: Int -> IO (DwfResult Double)
+analogInFrequencyGet p = fToDouble (fdwf_analog_in_frequency_get p')
+    where p' = fromIntegral p
+
+analogInRecordLengthSet :: Int -> Double -> IO (DwfResult ())
+analogInRecordLengthSet p q = fCall (fdwf_analog_in_record_length_set p' q')
+    where p' = fromIntegral p
+          q' = coerce q :: CDouble
+
+analogInRecordLengthGet :: Int -> IO (DwfResult Double)
+analogInRecordLengthGet p = fToDouble (fdwf_analog_in_record_length_get p')
+    where p' = fromIntegral p
+
+analogInStatus :: Int -> Int -> IO (DwfResult Int)
+analogInStatus p q = fToUChar (fdwf_analog_in_status p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusSampleLeft :: Int -> IO (DwfResult Int)
+analogInStatusSampleLeft p = fToInt (fdwf_analog_in_status_samples_left p')
+    where p' = fromIntegral p
+
+analogInStatusSamplesValid :: Int -> IO (DwfResult Int)
+analogInStatusSamplesValid p = fToInt (fdwf_analog_in_status_samples_valid p')
+    where p' = fromIntegral p
+
+analogInStatusIndexWrite :: Int -> IO (DwfResult Int)
+analogInStatusIndexWrite p = fToInt (fdwf_analog_in_status_index_write p')
+    where p' = fromIntegral p
+
+analogInStatusAutoTriggered :: Int -> IO (DwfResult Int)
+analogInStatusAutoTriggered p = fToInt (fdwf_analog_in_status_auto_triggered p')
+    where p' = fromIntegral p
+
+analogInStatusData :: Int -> Int -> Int -> IO (DwfResult [Double])
+analogInStatusData p q r = fToDoubleArrayN r (fdwf_analog_in_status_data p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusData2 :: Int -> Int -> Int -> Int -> IO (DwfResult [Double])
+analogInStatusData2 p q i n = fToDoubleArrayIN i n (fdwf_analog_in_status_data2 p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusData16 :: Int -> Int -> Int -> Int -> IO (DwfResult [Int])
+analogInStatusData16 p q i n = fToIntArrayIN i n (fdwf_analog_in_status_data16 p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusNoise :: Int -> Int -> Int -> IO (DwfResult [(Double, Double)])
+analogInStatusNoise p q n = fTo2DoubleArrayN n (fdwf_analog_in_status_noise p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusNoise2 :: Int -> Int -> Int -> Int -> IO (DwfResult [(Double, Double)])
+analogInStatusNoise2 p q i n = fTo2DoubleArrayIN i n (fdwf_analog_in_status_noise2 p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusSample :: Int -> Int -> IO (DwfResult Double)
+analogInStatusSample p q = fToDouble (fdwf_analog_in_status_sample p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInStatusTime :: Int -> IO (DwfResult (Int, Int, Int))
+analogInStatusTime p = fToIntIntInt (fdwf_analog_in_status_time p')
+    where p' = fromIntegral p
+
+analogInStatusRecord :: Int -> IO (DwfResult (Int, Int, Int))
+analogInStatusRecord p = fToIntIntInt (fdwf_analog_in_status_record p')
+    where p' = fromIntegral p
+
+analogInBitsInfo :: Int -> IO (DwfResult Int)
+analogInBitsInfo p = fToInt (fdwf_analog_in_bits_info $ fromIntegral p)
+
+analogInBufferSizeInfo :: Int -> IO (DwfResult (Int, Int))
+analogInBufferSizeInfo p = fToIntInt (fdwf_analog_in_buffer_size_info $ fromIntegral p)
+
+analogInBufferSizeGet :: Int -> IO (DwfResult Int)
+analogInBufferSizeGet p = fToInt (fdwf_analog_in_buffer_size_get $ fromIntegral p)
+
+analogInBufferSizeSet :: Int -> Int -> IO (DwfResult ())
+analogInBufferSizeSet p q = fCall (fdwf_analog_in_buffer_size_set p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInNoiseSizeInfo :: Int -> IO (DwfResult Int)
+analogInNoiseSizeInfo p = fToInt (fdwf_analog_in_noise_size_info $ fromIntegral p)
+
+analogInNoiseSizeSet :: Int -> Int -> IO (DwfResult ())
+analogInNoiseSizeSet p q = fCall (fdwf_analog_in_noise_size_set p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInAcquisitionModeInfo :: Int -> IO (DwfResult Int)
+analogInAcquisitionModeInfo p = fToInt (fdwf_analog_in_acquisition_mode_info $ fromIntegral p)
+
+analogInAcquisitionModeGet :: Int -> IO (DwfResult Int)
+analogInAcquisitionModeGet p = fToInt (fdwf_analog_in_acquisition_mode_get $ fromIntegral p)
+
+analogInChannelRangeInfo :: Int -> IO (DwfResult (Double, Double, Double))
+analogInChannelRangeInfo p = fToDoubleDoubleDouble (fdwf_analog_in_channel_range_info p')
+    where p' = fromIntegral p
+
+analogInChannelRangeSteps :: Int -> IO (DwfResult [Double])
+analogInChannelRangeSteps p = fToIntDoubleArray32 (fdwf_analog_in_channel_range_steps p')
+    where p' = fromIntegral p
+
+analogInChannelRangeGet :: Int -> Int -> IO (DwfResult Double)
+analogInChannelRangeGet p q = fToDouble (fdwf_analog_in_channel_range_get p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInChannelRangeSet :: Int -> Int -> Double -> IO (DwfResult ())
+analogInChannelRangeSet p q r = fCall (fdwf_analog_in_channel_range_set p' q' r')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+          r' = coerce r
+
+analogInChannelOffsetInfo :: Int -> IO (DwfResult (Double, Double, Double))
+analogInChannelOffsetInfo p = fToDoubleDoubleDouble (fdwf_analog_in_channel_offset_info p')
+    where p' = fromIntegral p
+
+analogInChannelOffsetGet :: Int -> Int -> IO (DwfResult Double)
+analogInChannelOffsetGet p q = fToDouble (fdwf_analog_in_channel_offset_get p' q')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+
+analogInChannelOffsetSet :: Int -> Int -> Double -> IO (DwfResult ())
+analogInChannelOffsetSet p q r = fCall (fdwf_analog_in_channel_offset_set p' q' r')
+    where p' = fromIntegral p
+          q' = fromIntegral q
+          r' = coerce r
+
